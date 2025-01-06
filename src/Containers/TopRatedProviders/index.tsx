3bc8280dff8c219cbd03ad4332847d6de35b658e
Suhaila <suhailathalappill@gmail.com>
2025-01-06 15:38:24 +0530

    feature: top restaurants in user

---
+++ src/Containers/TopRatedProviders/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import TopProvidersComponent from "../../Components/TopRatedProviders";
+import { fetchApprovedProviderDetails } from "../../Services/ProviderService";
+import { Provider } from "../../Components/TopRatedProviders/type";
+import { Box, CircularProgress, Typography } from "@mui/material";
+
+function TopProvidersContainer() {
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      try {
+        const response = await fetchApprovedProviderDetails();
+        setProviders(response.result);
+      } catch (err) {
+        setError("Failed to fetch providers");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (loading)
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  if (error) return <Typography color="error">{error}</Typography>;
+  return <TopProvidersComponent providers={providers} />;
+}
+
+export default TopProvidersContainer;
