1d11539631904330eb08467a313332dd596e34d8
shahana <tshahana2894@gmail.com>
2025-01-15 15:03:49 +0530

    google map in payment details

--- src/Components/Order/index.tsx
+++ src/Components/Order/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-import { Box, Typography, Button, CircularProgress } from "@mui/material";
-import InputField from "../../Atoms/Input";
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 import { useLocation } from "react-router-dom";
+import L from "leaflet";
+import InputField from "../../Atoms/Input";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface OrderFormData {
   formData: {
@@ -11,7 +29,6 @@ interface OrderFormData {
     city: string;
   };
   setSelectedCategories: React.Dispatch<React.SetStateAction<string[]>>;
-  
   setOrderId: React.Dispatch<React.SetStateAction<string | undefined>>;
   setDate: React.Dispatch<React.SetStateAction<string | undefined>>;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -35,12 +52,76 @@ const OrderComponent: React.FC<OrderFormData> = ({
   setDate,
   loading,
 }) => {
+  const [selectedCity, setSelectedCity] = useState(formData.city || "");
+  const [mapPosition, setMapPosition] = useState<[number, number] | null>(null);
+  
+  const user = localStorage.getItem("user");
   const { state } = useLocation();
   const { categories, orderId, date } = state || {};
   setSelectedCategories(categories);
   setOrderId(orderId);
   setDate(date);
-  const user=localStorage.getItem("user");
+
+
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          setMapPosition([latitude, longitude]);
+
+          const provider = new OpenStreetMapProvider();
+          provider
+            .search({ query: `${latitude}, ${longitude}` })
+            .then((results) => {
+              if (results[0]) {
+                const city = results[0].label;
+                setSelectedCity(city);
+
+                handleChange({
+                  target: {
+                    name: "city",
+                    value: city,
+                  },
+                } as React.ChangeEvent<HTMLInputElement>);
+              }
+            });
+        },
+        (error) => {
+          console.error("Error getting location: ", error);
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const LocationMarker = () => {
+    useMapEvents({
+      click(e) {
+        setMapPosition([e.latlng.lat, e.latlng.lng]);
+
+        const provider = new OpenStreetMapProvider();
+        provider
+          .search({ query: `${e.latlng.lat}, ${e.latlng.lng}` })
+          .then((results) => {
+            if (results[0]) {
+              setSelectedCity(results[0].label);
+
+              handleChange({
+                target: {
+                  name: "city",
+                  value: results[0].label,
+                },
+              } as React.ChangeEvent<HTMLInputElement>);
+            }
+          });
+      },
+    });
+
+    return mapPosition ? <Marker position={mapPosition}></Marker> : null;
+  };
 
   return (
     <Box
@@ -58,7 +139,7 @@ const OrderComponent: React.FC<OrderFormData> = ({
         Payment details
       </Typography>
 
-      {/* Form  */}
+      {/* Form */}
       <Box
         component="form"
         onSubmit={handleSubmit}
@@ -72,7 +153,7 @@ const OrderComponent: React.FC<OrderFormData> = ({
           variant="outlined"
           fullWidth
         />
-       
+
         <InputField
           label="Address"
           name="address"
@@ -85,10 +166,25 @@ const OrderComponent: React.FC<OrderFormData> = ({
         <Typography variant="body2" color="error">
           {errors.address}
         </Typography>
+
+       
+       
+        <MapContainer
+          center={[51.505, -0.09]} 
+          zoom={13}
+          style={{ height: "300px", width: "100%", marginTop: "10px" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <LocationMarker />
+        </MapContainer>
+
         <InputField
           label="Current Location"
           name="city"
-          value={formData.city}
+          value={selectedCity || formData.city}
           onChange={handleChange}
           required
           variant="outlined"
@@ -97,6 +193,24 @@ const OrderComponent: React.FC<OrderFormData> = ({
         <Typography variant="body2" color="error">
           {errors.city}
         </Typography>
+        <Button
+          type="button"
+          variant="contained"
+          sx={{
+            marginTop: 2,
+            backgroundColor: "#f97f29",
+            color: "#fff",
+            "&:hover": { backgroundColor: "#f96c0a" },
+          }}
+          onClick={getCurrentLocation} 
+        >
+          {loading ? (
+            <CircularProgress size={24} sx={{ color: "#fff" }} />
+          ) : (
+            "Get Current Location"
+          )}
+        </Button>
+
         <InputField
           label="Phone Number"
           name="ph_no"
@@ -109,6 +223,9 @@ const OrderComponent: React.FC<OrderFormData> = ({
         <Typography variant="body2" color="error">
           {errors.ph_no}
         </Typography>
+
+       
+
         <Button
           type="submit"
           variant="contained"
